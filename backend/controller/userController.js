f8d47295e4f1edf40a943e79bb806844e0488d0d
spitliner <>
2023-11-04 13:38:35 +0700

    change user schema

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,7 +3,8 @@ export const checkEmail = async (req, res) => {
 }
 
 export const CreateUser = async (req, res) => {
-
+    let { email, username, password } = req.body;
+    
 }
 
 export const Login = async (req, res) => {
@@ -11,5 +12,7 @@ export const Login = async (req, res) => {
 }
 
 export const verifyuser = async (req, res) => {
-
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: 'No credentials sent!' });
+    }
 }
